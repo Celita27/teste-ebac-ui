8d6be1ab92ab773d49c589af793afc30f2f44435
Celita27 <clfonseca.36@gmail.com>
2024-06-10 18:59:45 -0300

    primeiro teste automatizado

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,15 +1,13 @@
-///<reference types='cypress'/>
+///<reference types= "cypress"/>
 
-describe('Funcionalidade: Login', ()=> { }
-    
-it ('Deve fazer login com sucesso', () => {
-     cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
-     cy.get('#username') .type ('celita27.teste@teste.com.br')
-     cy.get('#password').type('271518vini')
-     cy.get('.woocommerce-form >.button').click()
-     cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain','Olá, celita27.teste (não é celita27.teste? Sair)')
+describe('Funcionalidade Login',() => {
 
-}) 
+    it('Deve fazer login com sucesso',() => {
+    cy.visit('http://lojaebac.ebaconline.art.br/minha conta')
+    cy.get('#username').type('celita27.teste@teste.com.br')
+    cy.get('#password').type ('271518vini')
+    cy.get('.woocommerce-form > .button').click()    
+    cy.get('.woocommerce-MyAccount-content > :nth-child(2)').should('contain','Olá, celita27.teste (não é celita27.teste? Sair)')
 
-
+})
 })
